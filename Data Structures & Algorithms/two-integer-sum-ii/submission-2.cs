8b12b5ba88d43ec362c21461536ea327c27872f9
marc-hg <90116640+marc-hg@users.noreply.github.com>
2026-04-04 09:52:33 +0200

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.Length - 1;
+        while (l < r)
+        {
+            var left = numbers[l];
+            var right = numbers[r];
+            if (left + right > target)
+            {
+                r--;
+                continue;
+            }
+
+            if (left + right < target)
+            {
+                l++;
+                continue;
+            }
+            if (left + right == target) return [l+1, r+1];
+
+        }
+        return [-1,-1];
+    }
+}
