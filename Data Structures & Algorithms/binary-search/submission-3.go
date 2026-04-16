28dd609623b4ed5846a4c9fd2c7b344ca824f343
marc-hg <90116640+marc-hg@users.noreply.github.com>
2026-04-16 22:37:37 +0200

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high - low) / 2
+		if target == nums[mid] {
+			return mid
+		}
+
+		if target > nums[mid] {
+			low = mid + 1
+		} else  {
+			high = mid - 1
+		}
+	}
+	return -1 
+}
