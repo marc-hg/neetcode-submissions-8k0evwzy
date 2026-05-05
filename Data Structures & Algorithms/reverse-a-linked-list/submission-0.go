567ca3561d91c70c527c11d070ccd825f7c8ca4f
marc-hg <90116640+marc-hg@users.noreply.github.com>
2026-05-05 23:37:20 +0200

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	currentNode := head
+	var prevNode *ListNode
+	for currentNode != nil {
+		nextNode := currentNode.Next
+		currentNode.Next = prevNode
+
+		prevNode = currentNode
+		currentNode = nextNode
+
+	}
+
+	return prevNode
+}
